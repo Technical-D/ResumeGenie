56de3f9e7dd325a2d52ea39c006f905763097124
Dheeraj Gupta <dheerajgupta1902@gmail.com>
2025-01-25 21:13:20 +0530

    feat:nlp-added

--- backend/parser.py
+++ backend/parser.py
@@ -1,5 +1,14 @@
 import fitz
 import re
+import spacy
+import json
+
+# Loading Countries and cities
+with open('country_city_mapping.json', 'r', encoding="utf-8") as f:
+    data = dict(json.load(f))
+
+countries = list(data.keys())
+indian_cities = list(data['India'])
 
 def extract_text_from_pdf(file_path):
     """
@@ -12,20 +21,54 @@ def extract_text_from_pdf(file_path):
         text += page.get_text()
     return text
 
+def validate_name(name):    
+    return len(name.split()) >= 2 and all(part.isalpha() for part in name.split())
+
+def validate_city(city):
+    if city.lower() in [c.lower() for c in indian_cities]:
+        return True
+    return False
+
+def validate_country(country):
+    if country.lower() in [c.lower() for c in countries]:
+        return True
+    return False
+
+def extract_info_using_nlp(text):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    info = {"name": None, "city": None, "country": None}
+    for ent in doc.ents:
+        if ent.label_ == "PERSON" and validate_name(ent.text):
+            info["name"] = ent.text
+        
+        if ent.label_ == "GPE":
+            if validate_city(ent.text):
+                info["city"] = ent.text
+
+            if validate_country(ent.text):
+                info["country"] = ent.text
+        
+    return info
+
 def extract_resume_data(text):
     """
-    Extract structured data (like name, email, phone number) from the resume text
+    Extract structured data from the resume text
     """
     email_pattern = r"[a-zA-Z0-9._%+-]+@[a-zACharacters0-9.-]+\.[a-zA-Z]{2,}"
     phone_pattern = r"\+?[\d\(\)\- ]{10,15}"
 
+    name, city, country= extract_info_using_nlp(text).values()
     email = re.search(email_pattern, text)
     phone = re.search(phone_pattern, text)
 
     data =  {
+        "name" : name,
         "email": email.group(0) if email else None,
         "phone": phone.group(0) if phone else None,
-        "text" : text
+        "city" : city,
+        "country": country
+        # "text" : text
     }
     return data
 
